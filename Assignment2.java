7cbd411dc01d6c46fd9ac03163011ed23e8906bc
ts-sudhish <sudhish.soss@gmail.com>
2016-06-15 13:23:03 +0000

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package sdh.java.session1;
+
+import java.util.Scanner;
+
+/**
+ * @author sudhi
+ * 
+ */
+public class Assignment2 {
+	public static void main(String[] args) {
+		int a, b = 4, c;
+		a = 0;
+		c = 0;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+
+	}
+
+}
